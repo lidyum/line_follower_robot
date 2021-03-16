81628b9f4bb3efc8c587d2a334a926652eff3622
erhan <e.koclar@betik.com.tr>
2021-03-16 19:45:23 +0300

    create test_pwm file

---
+++ test_pwm.py
@@ -0,0 +1,19 @@
+import RPi.GPIO as GPIO
+from time import sleep
+
+PIN_LED = 12
+GPIO.setmode(GPIO.BOARD)  # set pin numbering system
+GPIO.setup(PIN_LED, GPIO.OUT)
+pi_pwm = GPIO.PWM(PIN_LED, 1000)  # 1Khz
+pi_pwm.start(0)  # start PWM of required Duty Cycle
+
+while True:
+    for duty in range(0, 101, 1):
+        pi_pwm.ChangeDutyCycle(duty)
+        sleep(0.01)
+    sleep(0.5)
+
+    for duty in range(100, -1, -1):
+        pi_pwm.ChangeDutyCycle(duty)
+        sleep(0.01)
+    sleep(0.5)
