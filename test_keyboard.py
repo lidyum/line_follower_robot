c2442da54b579a68a5e886a5812a2032fcf42ed2
erhan <e.koclar@betik.com.tr>
2021-03-16 20:18:06 +0300

    change key capture method

--- test_keyboard.py
+++ test_keyboard.py
@@ -1,8 +1,31 @@
-import time
+import curses
 
-from pip._vendor.distlib.compat import raw_input
+# Get the curses window, turn off echoing of keyboard to screen, turn on
+# instant (no waiting) key response, and use special values for cursor keys
+screen = curses.initscr()
+curses.noecho()
+curses.cbreak()
+screen.keypad(True)
 
-while True:
-    key = raw_input()
-    print(key)
-    time.sleep(2)
+try:
+    while True:
+        char = screen.getch()
+        if char == ord('q'):
+            break
+        elif char == curses.KEY_UP:
+            print("up")
+        elif char == curses.KEY_DOWN:
+            print("down")
+        elif char == curses.KEY_RIGHT:
+            print("right")
+        elif char == curses.KEY_LEFT:
+            print("left")
+        elif char == 10:
+            print("stop")
+
+finally:
+    # Close down curses properly, inc turn echo back on!
+    curses.nocbreak();
+    screen.keypad(0);
+    curses.echo()
+    curses.endwin()
