731acbdddaa6cf646e3f67f5a41090cdb269e7dc
erhan <e.koclar@betik.com.tr>
2021-03-17 12:11:05 +0300

    update car control for right motor

--- control_car.py
+++ control_car.py
@@ -2,14 +2,19 @@ import curses
 
 import RPi.GPIO as GPIO
 
-MOTOR_LEFT_IN1 = 38
-MOTOR_LEFT_IN2 = 40
-GPIO.setmode(GPIO.BOARD)  # set pin numbering system
-GPIO.setup(MOTOR_LEFT_IN1, GPIO.OUT)
-GPIO.setup(MOTOR_LEFT_IN2, GPIO.OUT)
+MOTOR_RIGHT_IN1 = 29
+MOTOR_RIGHT_IN2 = 31
+MOTOR_RIGHT_ENABLE = 33
 
-GPIO.output(MOTOR_LEFT_IN1, GPIO.LOW)
-GPIO.output(MOTOR_LEFT_IN2, GPIO.LOW)
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(MOTOR_RIGHT_IN1, GPIO.OUT)
+GPIO.setup(MOTOR_RIGHT_IN2, GPIO.OUT)
+PWM_RIGHT_ENABLE = GPIO.PWM(MOTOR_RIGHT_ENABLE, 1000)  # 1Khz
+PWM_RIGHT_ENABLE.start(0)
+
+GPIO.output(MOTOR_RIGHT_IN1, GPIO.LOW)
+GPIO.output(MOTOR_RIGHT_IN2, GPIO.LOW)
+PWM_RIGHT_ENABLE.ChangeDutyCycle(0)
 
 # Get the curses window, turn off echoing of keyboard to screen, turn on
 # instant (no waiting) key response, and use special values for cursor keys
@@ -28,22 +33,23 @@ try:
             break
         elif char == curses.KEY_UP:
             print("up")
-            GPIO.output(MOTOR_LEFT_IN1, GPIO.HIGH)
-            GPIO.output(MOTOR_LEFT_IN2, GPIO.LOW)
+            GPIO.output(MOTOR_RIGHT_IN1, GPIO.HIGH)
+            GPIO.output(MOTOR_RIGHT_IN2, GPIO.LOW)
+            PWM_RIGHT_ENABLE.ChangeDutyCycle(50)
         elif char == curses.KEY_DOWN:
             print("down")
-            GPIO.output(MOTOR_LEFT_IN1, GPIO.LOW)
-            GPIO.output(MOTOR_LEFT_IN2, GPIO.HIGH)
+            GPIO.output(MOTOR_RIGHT_IN1, GPIO.LOW)
+            GPIO.output(MOTOR_RIGHT_IN2, GPIO.HIHG)
+            PWM_RIGHT_ENABLE.ChangeDutyCycle(50)
         elif char == curses.KEY_RIGHT:
             print("right")
         elif char == curses.KEY_LEFT:
             print("left")
-        elif char == 10:
-            print("stop")
         else:
             print("stop motor")
-            GPIO.output(MOTOR_LEFT_IN1, GPIO.LOW)
-            GPIO.output(MOTOR_LEFT_IN2, GPIO.LOW)
+            GPIO.output(MOTOR_RIGHT_IN1, GPIO.LOW)
+            GPIO.output(MOTOR_RIGHT_IN2, GPIO.LOW)
+            PWM_RIGHT_ENABLE.ChangeDutyCycle(0)
 
 finally:
     # Close down curses properly, inc turn echo back on!
@@ -51,3 +57,7 @@ finally:
     screen.keypad(0);
     curses.echo()
     curses.endwin()
+
+    GPIO.output(MOTOR_RIGHT_IN1, GPIO.LOW)
+    GPIO.output(MOTOR_RIGHT_IN2, GPIO.LOW)
+    PWM_RIGHT_ENABLE.ChangeDutyCycle(0)
