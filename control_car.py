d0a5809247459366f9ae87ad76bbb6039895d983
erhan <e.koclar@betik.com.tr>
2021-03-19 19:28:08 +0300

    reset time

--- control_car.py
+++ control_car.py
@@ -100,9 +100,6 @@ try:
                 else:
                     is_right_on_line = GPIO.input(SENSOR_RIGHT)
                     is_left_on_line = GPIO.input(SENSOR_LEFT)
-                    print("right_sensor_on_line", is_right_on_line)
-                    print("left_sensor_on_line", is_left_on_line)
-                    time.sleep(2)
                     if is_right_on_line:
                         left_motor_forward()
                         right_motor_stop()
